32fcb1394814c2adbac3a933e09413f8a0bdfbba
holis <holisfei@gmail.com>
2026-05-13 13:02:50 +0800

    feat：complete type hints, generics, closures with mypy strict mode

---
+++ src/02 typing.py
@@ -0,0 +1,106 @@
+from typing import Union, Optional, Any, TypeVar, Callable
+
+# 字符
+name: str = "str"
+# 数字
+age: int = 18
+weight: float = 60.56
+# bool
+isLight: bool = False
+# 空值，变量是str类型或者空值
+empty: None | str = None
+
+# 数组
+numbers: list[int] = [1, 2, 3, 4]
+# 字典
+maps: dict[str, str] = {"name":"bob", "age":"18"}
+# 集合
+sets: set[str] = {"a", "b"}
+# 定长元祖
+tuples: tuple[int,float,int,int] = (1, 1.23, 3, 2)
+# 不定长元祖
+tuples2: tuple[int,...] = (1, 2, 3, 4, 5, 6)
+
+# Optional 可选
+timeout: Optional[float] = None
+timeout = 1.1;
+
+# Union
+user_id: Union[int, str, float] = 1
+user_id1: int | str | float = 1
+
+# Any
+value: Any = None
+value = "one"
+
+# [T] 泛型
+def get_first_item[T](data:list[T]) -> T:
+    return data[0]
+get_first_item(data=[1, 2, 3])
+get_first_item(data=["one", "two", "three"])
+# 泛型约束
+def add[T: (int | float)](p1: T, p2: T) -> T:
+    return p1 + p2
+sum_int = add(p1=1, p2=2)
+sum_float = add(p1=1.1, p2=1.2)
+print(f"泛型约束：{sum_int}")
+print(f"泛型约束：{sum_float}")
+
+
+# Callable[[参数], 返回值] 闭包类型注解 
+on_click = Callable[[int, str], bool]
+# Callable[[int, str], bool] ->   对应swift的 (int, str) -> bool
+# Callable[[], bool]         ->   对应swift的 () -> bool
+# Callable[[int], bool]      ->   对应swift的 (int) -> bool
+# Callable[..., bool]        ->   任意参数
+
+# 闭包函数
+def make_counter(count: int) -> Callable[[], int]:
+    number: int = 0
+    def counter() -> int:
+        nonlocal number # nonlocal表示捕获外部的变量，而不是新创建的变量
+        number+=1
+        return number
+    return counter # 返回闭包函数的声明，并没有执行函数
+counter = make_counter(count=1) # 返回闭包
+res = counter() # 调用闭包
+print(f"闭包函数：{res}")
+
+# Lambda：匿名闭包
+# 常作为简短表达式
+lamb = lambda x: x*2
+print(f"Lambda：{lamb(2)}")
+# Lambda作为参数
+sort_res = sorted([3, 1, 4, 1, 5, 9, 2, 6], key=lambda x : -x) # 降序排列
+print(f"Lambda排序：{sort_res}")
+
+# 例子
+def parse_configure(rawdata: str) -> dict[str, Union[str, int, bool]]:
+    configure: dict[str, Union[str, int ,bool]] = {}
+    # 按行分割原始字符
+    lines: list[str] = rawdata.splitlines()
+    for line in lines:
+        line = line.strip()
+        if not line: # 跳过空行
+            continue
+        # 用 = 分割字符串
+        key_p, _, value_p = line.partition("=")
+        key: str = key_p.strip()
+        value: str = value_p.strip()
+        if value.isdigit():
+            configure[key] = int(value)
+        elif value.lower() in ("true", "false"):
+            configure[key] = value.lower() == "true"
+        else:
+            configure[key] = value
+    return configure
+
+
+raw: str = """
+host = localhost
+port = 8080
+debug = true
+name = pythonLearning
+"""
+config = parse_configure(rawdata=raw)
+print(f"结果：{config}")
